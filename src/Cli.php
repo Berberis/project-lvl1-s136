27505b592455e1240448083a07b4757830a94e75
lumo <lumolilumo@gmail.com>
2017-07-15 19:06:40 +0300

    fixes from linter

--- src/Cli.php
+++ src/Cli.php
@@ -1,22 +1,12 @@
 <?php
 
 namespace BrainGames\Cli;
-/*
-function run()
-{
-echo 'Welcome to the Brain Game!', PHP_EOL;
-echo 'May I have your name? ';
-
-$name = trim(fgets(STDIN));
-echo "Hello, {$name}!", PHP_EOL;
-}
-*/
 
 use function \cli\line;
 
 function run()
 {
-  line('Welcome to the Brain Games!');
-  $name = \cli\prompt('May I have your name?');
-  line("Hello, %s!", $name);
+    line('Welcome to the Brain Games!');
+    $name = \cli\prompt('May I have your name?');
+    line("Hello, %s!", $name);
 }
