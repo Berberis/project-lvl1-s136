80e1786971c8a511c23c38384cc7cfb75a72ef55
lumo <lumolilumo@gmail.com>
2017-07-15 07:27:22 +0300

    rewrite run()

--- src/Cli.php
+++ src/Cli.php
@@ -1,7 +1,23 @@
 <?php
 
-  use function \cli\line;
+namespace BrainGames\Cli;
+/*
+function run()
+{
+echo 'Welcome to the Brain Game!', PHP_EOL;
+echo 'May I have your name? ';
 
+$name = trim(fgets(STDIN));
+echo "Hello, {$name}!", PHP_EOL;
+}
+*/
+///*
+use function \cli\line;
+
+function run()
+{
   line('Welcome to the Brain Games!');
   $name = \cli\prompt('May I have your name?');
   line("Hello, %s!", $name);
+}
+//*/
