9e6c66317edead097c0426d1c71c1dca214e26d3
lumo <lumolilumo@gmail.com>
2017-07-16 07:32:45 +0300

    add working version

--- src/Gcd.php
+++ src/Gcd.php
@@ -11,21 +11,25 @@ function gcd()
     $task = function () {
         $num1 = rand(1, 99);
         $num2 = rand(1, 99);
-        return ($num1, $num2);
-        }
-      };
-
-    $number = function () {
-        return rand(1, 99);
+        return $num1 . ' ' . $num2;
     };
-    function isEven($num)
+
+    function findGcd($num1, $num2)  //using Euclidean algorithm
     {
-        return ($num % 2) === 0;
+        while ($num1 <> 0 && $num2 <> 0) {
+          if ($num1 > $num2)
+            $num1 = $num1 % $num2;
+          else
+            $num2 = $num2 % $num1;
+        }
+        return abs($num1 + $num2);
     }
 
     $correctAnswer = function ($task) {
-        return calculate($num1, $num2);
+        list($num1, $num2) = explode(' ', $task);
+        return findGcd($num1, $num2);
     };
+    echo '$correctAnswer';
 
     universalOutput($RULES_OF_THE_GAME, $task, $correctAnswer);
 }
