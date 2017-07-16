24a722936e5724743871c49d368b5fbbf8c09a70
lumo <lumolilumo@gmail.com>
2017-07-16 06:59:19 +0300

    fix errors

--- src/Calc.php
+++ src/Calc.php
@@ -19,19 +19,19 @@ function calc()
             default:
                 return $num1 . ' - ' . $num2;
         }
-      };
+    };
 
-      function calculate($num1, $num2, $operation)
-      {
-          switch ($operation) {
-              case '+':
-                  return $num1 + $num2;
-              case '*':
-                  return $num1 * $num2;
-              default:
-                  return $num1 - $num2;
+    function calculate($num1, $num2, $operation)
+    {
+        switch ($operation) {
+            case '+':
+                return $num1 + $num2;
+            case '*':
+                return $num1 * $num2;
+            default:
+                return $num1 - $num2;
           }
-      }
+    }
 
     $correctAnswer = function ($task) {
         list($num1, $operation, $num2) = explode(' ', $task);
