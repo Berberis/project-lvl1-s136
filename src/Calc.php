4c98462ff1858fe992e7e4de2c425877fcdc2624
lumo <lumolilumo@gmail.com>
2017-07-16 05:59:53 +0300

    add demo version

--- src/Calc.php
+++ src/Calc.php
@@ -2,10 +2,42 @@
 
 namespace BrainGames\Calc;
 
-use function \cli\line;
-use function \BrainGames\Cli\run;
+use function \BrainGames\Cli\universalOutput;
 
 function calc()
 {
-    line('TEST CALC');
+    $RULES_OF_THE_GAME = 'What is the result of the expression?';
+
+    $task = function () {
+        $num1 = rand(1, 10);
+        $num2 = rand(1, 10);
+        switch (rand(0, 2)) {
+            case 0:
+                return $num1 . ' + ' . $num2;
+            case 1:
+                return $num1 . ' * ' . $num2;
+            default:
+                return $num1 . ' - ' . $num2;
+        }
+      };
+
+      function calculate($num1, $num2, $operation)
+      {
+          switch ($operation) {
+              case '+':
+                  return $num1 + $num2;
+              case '*':
+                  return $num1 * $num2;
+              case '-':
+                  return $num1 - $num2;
+              default:
+                  echo 'ERROR';
+                  return false;
+          }
+
+    $correctAnswer = function ($task) {
+        list($num1, $operation, $num2) = explode(' ', $task);
+        return calculate($num1, $num2, $operation);
+
+    universalOutput($RULES_OF_THE_GAME, $task, $correctAnswer);
 }
